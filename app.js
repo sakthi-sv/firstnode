d799f208b3c52fa2c12fef76c886755ab789528f
Sakthivel S <40761420+sakthi-sv@users.noreply.github.com>
2020-05-03 11:01:44 +0530

    sql connection successfull

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 //Require module
 const express = require('express');
+const bodyParser =require("body-parser");
 // Express Initialize
 const app = express();
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+const sql=require("./models/db.js");
+//server
 const port = 8000;
 app.listen(port,()=> {
 console.log('listen port 8000');
